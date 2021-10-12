cc925210c554eb8481b2939d57de1aee33f10981
09-AdrianAng <adrian_ang_ding_xiang@connect.ite.edu.sg>
2021-10-12 19:35:58 +0800

    added buttons for game win scene and coding for score points

---
+++ Assets/Scripts/CollisionCheck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionCheck : MonoBehaviour
+{
+    public GameObject scoreText;
+    public int theScore;
+
+    void OnTriggerEnter(Collider other)
+    {
+        theScore += 10;
+        scoreText.GetComponent<Text>().text = "Score: " + theScore;
+        Destroy(gameObject);
+    }
+}
