9c9e9641e77c332f0c3036648e080bc932297af9
09-AdrianAng <adrian_ang_ding_xiang@connect.ite.edu.sg>
2021-10-12 20:47:17 +0800

    Remodify everything

--- Assets/Scripts/CollisionCheck.cs
+++ Assets/Scripts/CollisionCheck.cs
@@ -2,14 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CollisionCheck : MonoBehaviour
 {
-    public GameObject scoreText;
-    public static int theScore;
+    public Text PointText;
+    public GameObject Effects;
+
+    private float TimerValue;
+    private float scorevalue;
+    public float totalcoins;
+    public float timeleft;
+
+    public int timeRemaining;
+
+    public Text Timetext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
 
     void Update()
     {
-        scoreText.GetComponent<Text>().text = "Score: " + theScore;
+        timeleft -= Time.deltaTime;
+        timeRemaining = Mathf.FloorToInt(timeleft % 60);
+        Timetext.text = "Timer: " + timeRemaining.ToString();
+
+        if (scorevalue == totalcoins)
+        {
+            if (timeleft <= TimerValue)
+            {
+                SceneManager.LoadScene("GameWinScene");
+            }
+        }
+        else if (timeleft <= 0)
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            scorevalue += 10;
+            PointText.text = "Points: " + scorevalue;
+            Destroy(other.gameObject);
+            Instantiate(Effects, other.transform.position, Quaternion.identity);
+        }
+        if (other.gameObject.tag == "Water")
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }
+    }
+
+
 }
