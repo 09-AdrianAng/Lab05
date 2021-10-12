176b40c5f38f7374cbcd8019ef4664807ae8e2cc
09-AdrianAng <adrian_ang_ding_xiang@connect.ite.edu.sg>
2021-10-12 20:16:50 +0800

    modify scripts v2

---
+++ Assets/Scripts/CollectCoin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        CollisionCheck.theScore += 10;
+        Destroy(gameObject);
+    }
+}
